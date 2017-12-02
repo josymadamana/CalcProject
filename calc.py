d4bc3ad8ab1ec628a36d131437668c7c9a82f6d4
josymadamana <josymadamana@gmail.com>
2017-12-02 14:31:37 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,6 +2,7 @@ def add(a,b):
 	return a+b
 def sub(a,b):
 	return a-b
+	#check 3
 def mul(a,b):
 	return a*b
 def div(a,b):
@@ -9,11 +10,13 @@ def div(a,b):
 		return a/b
 	except:
 		print "Exception Caught"
+		#check 1
 		raise
 if __name__=="__main__":
 	print(add(6,2))
 	print(sub(6,2))
 	print(mul(6,2))
+	#check 2
 	print(div(6,0))
 	#print("Done")		
 	#print("Done")		
