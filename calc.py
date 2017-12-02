b68dd59036c76ac32bd2ef37a8856b011ded5869
josymadamana <josymadamana@gmail.com>
2017-12-02 12:54:35 -0800

    calculator pgm added from pc

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add(a,b):
+	return a+b
+def sub(a,b):
+	return a-b
+def mul(a,b):
+	return a*b
+def div(a,b):
+	try:
+		return a/b
+	except:
+		print "Exception Caught"
+		raise
+if __name__=="__main__":
+	print(add(6,2))
+	print(sub(6,2))
+	print(mul(6,2))
+	print(div(6,0))
+		
+	
